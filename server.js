e172f8b074201da57aae025d8466c81ced116a3a
yakov <yakov@gmail.com>
2025-10-30 14:47:14 +0200

    fix: add utf-8 header for emoji support

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello DevOps 🚀\n");
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.end("Hello DevOps 🚀");
 });
 
 server.listen(port, hostname, () => {
